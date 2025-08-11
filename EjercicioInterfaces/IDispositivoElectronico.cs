103e8d713778ae05703e471a01e85139dcaff670
Fefemyster <kenennlara@gmail.com>
2025-08-11 00:38:02 -0600

    Add project files.

---
+++ EjercicioInterfaces/IDispositivoElectronico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioInterfaces
+{
+    public interface IDispositivoElectronico
+    {
+        public void Encender();
+        public void Apagar();
+        public void MostrarEstado();
+    }
+}
---
+++ EjercicioInterfaces/Telefono.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioInterfaces
+{
+    public class Telefono
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public bool EstaEncendido { get; set; }
+
+        public void Encender()
+        {
+            if (EstaEncendido == true)
+            {
+                Console.WriteLine("Esta encendido");
+            }
+        }
+
+    }
+}
